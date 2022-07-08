f237774c5fabed2422ce4a41692d94742ea6fb9b
Ntuthuko-yiga <yigasimon20@gmail.com>
2022-07-08 14:26:33 +0200

    initial commit

---
+++ pizza-cart-api.js
@@ -0,0 +1,24 @@
+document.addEventListener('alpine:init', () => {
+
+    Alpine.data('pizzaCartWithAPIWidget', function() {
+      return {
+
+        init() {
+
+            axios
+            .get('https://pizza-cart-api.herokuapp.com/api/pizzas')
+            .then((result) => {
+                // console.log(results.data);
+                this.pizzas = result.data.pizzas
+            })
+        },
+
+        message : 'Eating pizzas',
+
+        pizzas : []
+
+
+
+      }
+    })
+})
